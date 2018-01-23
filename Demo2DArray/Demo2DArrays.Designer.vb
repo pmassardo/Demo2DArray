dc77ebd55c7c28fb5efa3300cd45a3b4cecac933
Paul Massardo <paul.massardo@gmail.com>
2018-01-23 15:37:52 -0500

    Add project files.

---
+++ Demo2DArray/Demo2DArrays.Designer.vb
@@ -0,0 +1,113 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmDemo2DArrays
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.lbOutput = New System.Windows.Forms.Label()
+        Me.tbInput = New System.Windows.Forms.TextBox()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lbInput = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnReset
+        '
+        Me.btnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnReset.Location = New System.Drawing.Point(68, 89)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(51, 23)
+        Me.btnReset.TabIndex = 4
+        Me.btnReset.Text = "&Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'lbOutput
+        '
+        Me.lbOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lbOutput.Location = New System.Drawing.Point(11, 31)
+        Me.lbOutput.Name = "lbOutput"
+        Me.lbOutput.Size = New System.Drawing.Size(165, 55)
+        Me.lbOutput.TabIndex = 2
+        '
+        'tbInput
+        '
+        Me.tbInput.Location = New System.Drawing.Point(111, 8)
+        Me.tbInput.Name = "tbInput"
+        Me.tbInput.Size = New System.Drawing.Size(65, 20)
+        Me.tbInput.TabIndex = 1
+        Me.tbInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Location = New System.Drawing.Point(11, 89)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(51, 23)
+        Me.btnEnter.TabIndex = 3
+        Me.btnEnter.Text = "&Enter"
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnExit.Location = New System.Drawing.Point(125, 89)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(51, 23)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lbInput
+        '
+        Me.lbInput.Location = New System.Drawing.Point(8, 8)
+        Me.lbInput.Name = "lbInput"
+        Me.lbInput.Size = New System.Drawing.Size(45, 13)
+        Me.lbInput.TabIndex = 0
+        Me.lbInput.Text = "Enter #:"
+        '
+        'frmDemo2DArrays
+        '
+        Me.AcceptButton = Me.btnEnter
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnReset
+        Me.ClientSize = New System.Drawing.Size(186, 117)
+        Me.Controls.Add(Me.lbInput)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.lbOutput)
+        Me.Controls.Add(Me.tbInput)
+        Me.Controls.Add(Me.btnEnter)
+        Me.Controls.Add(Me.btnExit)
+        Me.Name = "frmDemo2DArrays"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "2D Arrays"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnReset As Button
+    Friend WithEvents lbOutput As Label
+    Friend WithEvents tbInput As TextBox
+    Friend WithEvents btnEnter As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lbInput As Label
+End Class
---
+++ Demo2DArray/Demo2DArrays.vb
@@ -0,0 +1,151 @@
+﻿''' <summary>
+''' Author:         Alfred Massardo
+''' Project Name:   Demo2DArrays
+''' Date:           23-Jan-2018
+''' Description:    Simple program to demonstrate creating, loading, looping, and display the content of a 2 dimensional array.
+''' </summary>
+Public Class frmDemo2DArrays
+
+    ' Class Level Constants
+    Private Const numberColumnLastIndex As Integer = 1  ' what is the last column index for the 2d array
+    Private Const numberRowLastIndex As Integer = 2     ' what is the last row index for the 2d array
+
+
+    ' Class Level Variabl3
+    ' 2 dimensional array
+    ' 2 columns = 1
+    ' 3 rows = 2
+    Private numberArray(numberRowLastIndex, numberColumnLastIndex) As Double
+
+    Private currentColumnIndex As Integer = 0  ' used to maintain the current row index
+    Private currentRowIndex As Integer = 0     ' used to maintain the current row index
+
+    ''' <summary>
+    ''' btnEnter_Click - enters the number into the 2D array. The location is depends upon the number of times the user has entered
+    '''                  entered a number, until the array is full. Once the 2D array is full a separate sub is called to display 
+    '''                  the array contents in the output label.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+
+        ' input the number entered into the array
+        ' no validation to simplify the code
+        numberArray(currentRowIndex, currentColumnIndex) = Convert.ToDouble(tbInput.Text)
+
+        ' if the row lindex is less than the last index
+        ' and the column counter is the last column
+        ' move to the next row and reset the column to the start
+        If currentRowIndex < numberRowLastIndex And currentColumnIndex = numberColumnLastIndex Then
+
+            'move to the next row
+            currentRowIndex += 1
+
+            'reset to the first column index
+            currentColumnIndex = 0
+
+            '' add carriage return line feed 
+            '' to move to the next row
+            'lbOutput.Text += vbCrLf
+
+        ElseIf currentRowIndex = numberRowLastIndex And currentColumnIndex = numberColumnLastIndex Then
+            ' if this is the last row and last index
+
+            ' disable the control so the user
+            ' cannot enter any more numbers
+            btnEnter.Enabled = False
+            tbInput.Enabled = False
+
+        Else
+
+            ' increment the column counter
+            currentColumnIndex += 1
+
+        End If
+
+        ' clear the input text box
+        tbInput.Text = String.Empty
+
+        ' sub to display the content of the array
+        ' in the output label
+        DisplayArrayContents(numberArray)
+
+    End Sub
+    ''' <summary>
+    ''' DisplayArrayContents - sub to display the content of the array passed to it.
+    ''' </summary>
+    ''' <param name="arrayParameter"></param>
+    Private Sub DisplayArrayContents(arrayParameter(,) As Double)
+
+        ' clear the output box 
+        ' to display the new values
+        lbOutput.Text = String.Empty
+
+        ' loop through all the rows
+        For rowIndex As Integer = 0 To arrayParameter.GetLength(0) - 1 ' this looks at the length of the 1st dimension or the row index
+
+            ' And for each row loops through all the columns in that row
+            For columnIndex As Integer = 0 To arrayParameter.GetLength(1) - 1 ' this looks at the length of the 2nd dimension or the column index
+
+                ' get the value at the interection of the specific row and column
+                ' and append a space to the end to create a separation between
+                ' entries
+                lbOutput.Text += arrayParameter(rowIndex, columnIndex).ToString() & " "
+
+            Next columnIndex ' next column index
+
+            lbOutput.Text += vbCrLf ' between rows append a carriage return line feed 
+
+        Next rowIndex ' next row index
+
+        ' 2 4
+        ' 6 8
+        ' 10 12
+
+    End Sub
+
+    ''' <summary>
+    ''' ResetForm sub that clears/resets the form
+    ''' </summary>
+    ''' <param name="arrayParameter"></param>
+    Private Sub ResetForm(arrayParameter(,) As Double)
+
+        ' loop through each row
+        For rowIndex As Integer = 0 To arrayParameter.GetLength(0) - 1
+
+            ' loop through each column
+            For columnIndex As Integer = 0 To arrayParameter.GetLength(1) - 1
+
+                ' set the value to zero
+                arrayParameter(rowIndex, columnIndex) = 0
+
+            Next columnIndex ' next column
+
+        Next rowIndex ' next row
+
+        ' reset form/class level variables
+        currentColumnIndex = 0   ' reset the input column counter
+        currentRowIndex = 0      ' reset the input row counter
+
+        lbOutput.Text = String.Empty    ' clear the output label
+
+        ' enable the enter button
+        btnEnter.Enabled = True ' endble the enter button 
+        tbInput.Enabled = True  ' endble the input text box 
+        tbInput.Focus()         ' set the focus to the input text box
+
+
+    End Sub
+
+    ''' <summary>
+    ''' btnReset_Click - event that resets the form
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+
+        ' Call the reset form sub
+        ResetForm(numberArray)
+
+    End Sub
+End Class
